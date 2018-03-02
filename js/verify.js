c98c6af46a31ed8522e91cb0065c73e3f3b03d96
amubawrnaw <amubawrnaw@gmail.com>
2018-03-03 01:14:21 +0800

    improved cookie verification

--- js/verify.js
+++ js/verify.js
@@ -1,12 +1,11 @@
 //check if user alr logged in
 var user = document.cookie.split("=")[1];
 var path = window.location.href;
-var file = path.split("/")[3];
 console.log(file + " <");
 if(user!=null){
-    if(file.indexOf("index.html")!= -1 || file == ""){
+    if(path.indexOf("index.html")!= -1 || path == ""){
         window.location.replace('landing.html');
     }
-}else if(file.indexOf("landing.html")!=-1){
+}else if(path.indexOf("landing.html")!=-1){
     window.location.replace('index.html');
 }
